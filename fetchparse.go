5107dbd7008dc1b0f5c351696b93304716399b99
Big Fun Cloud TODO <tim@tmickel.com>
2021-05-02 14:18:38 +0000

    Automated from Big Fun Cloud: M  fetchparse.go

--- fetchparse.go
+++ fetchparse.go
@@ -45,7 +45,21 @@ func FetchTable() ([]*Entry, error) {
 			return nil, err
 		}
 		entry := &Entry{
-			CodePoint: record[0],
+			CodePoint:                     record[0],
+			CharacterName:                 record[1],
+			GeneralCategory:               record[2],
+			CanonicalCombiningClasses:     record[3],
+			BidirectionalCategory:         record[4],
+			CharacterDecompositionMapping: record[5],
+			DecimalDigitValue:             record[6],
+			DigitValue:                    record[7],
+			NumericValue:                  record[8],
+			Mirrored:                      record[9],
+			Unicode1Name:                  record[10],
+			ISO10646CommentField:          record[11],
+			UppercaseMapping:              record[12],
+			LowercaseMapping:              record[13],
+			TitlecaseMapping:              record[14],
 		}
 		entries = append(entries, entry)
 	}
